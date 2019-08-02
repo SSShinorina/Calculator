732d8b85adc50f0350ba141bce1a8f6205c8a53b
Shinorina Shahrin Shaon <shaon20aust@gmail.com>
2019-08-02 17:42:11 +0600

    Parameter types and return type change to double from integer

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@ public class Claculator{
     public static void main(String[] args){
 
     }
-    int addition(int x, int y){
+    double addition(double x, double y){
         return x+y;
     }
 }
